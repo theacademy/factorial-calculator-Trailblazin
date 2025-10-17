ea663f723dd003f8bd076dc72dbff2e3fa2bfabd
Seun Osikomaiya <44784626+Trailblazin@users.noreply.github.com>
2025-10-17 17:52:19 +0100

    Final commit, completed working

--- factorial/src/main/java/com/wiley/assignment1/Factorial.java
+++ factorial/src/main/java/com/wiley/assignment1/Factorial.java
@@ -6,19 +6,18 @@ import java.util.Scanner;
 
 
 public class Factorial {
-	
-	 private Scanner scanner;
-	 private PrintStream printStream;
-	 private int num;
-	 
 
-	 public Factorial(InputStream inputStream, PrintStream printStream) {
-	      this.scanner = new Scanner(inputStream);
-	      this.printStream = printStream;
-	 }
-	 
-	 public int isInputNumberValid()  {
-		 
+    private Scanner scanner;
+    private PrintStream printStream;
+    private int num;
+
+
+    public Factorial(InputStream inputStream, PrintStream printStream) {
+        this.scanner = new Scanner(inputStream);
+        this.printStream = printStream;
+    }
+
+    public int isInputNumberValid() {
 		/*
 		- Update this method to accept a number from the user and store the value in an instance variable `num`. 
 			- Use an instance variable `scanner` to accept the input from the user.
@@ -33,35 +32,78 @@ public class Factorial {
 		- All code should be inside the placeholders below.
 		*/
 
-		//YOUR CODE STARTS HERE
+        //YOUR CODE STARTS HERE
+        this.printStream.println("Enter a value between 1 & 10: \n");
 
-		return -1;
+        if (this.scanner.hasNextInt()) {
+            //num holds the user input
+            num = this.scanner.nextInt();
+            if (1 <= num && num <= 10) {
+                //valid user input :D
+                return num;
+            }
+            //User's input was outside the requested bounds
+            else {
+                this.printStream.print("Invalid entry. Please enter an integer between 1 and 10, inclusive.");
+            }
+        }
+        //User's input was not a valid integer value
+        else {
+            this.printStream.print("Invalid entry. Please enter an integer between 1 and 10, inclusive.");
+        }
 
-		//YOUR CODE ENDS HERE
-		 
-			
-	 }
-	
-	 
-	 private void calculateFactorial(int num) {
+        return -1;
+        //YOUR CODE ENDS HERE
+
+    }
+
+
+    private void calculateFactorial(int num) {
 		
 		  /*
 		  Use this method to calculate the factorial of the number input by the user.
 		  
 		  - All code should be inside the placeholders below.
 		*/
-		 
-		int result = 1;
-		//YOUR CODE STARTS HERE
 
- 
+        int result = 1;
+        //YOUR CODE STARTS HERE
 
-		//YOUR CODE ENDS HERE
-		this.printStream.print("The Factorial is: " + result);		
-		
-	 }
-	 
-	 public void calculateFactorial() {
+        /*
+        Factorial formulae: n! = n * (n-1) * (n-2) ... 1
+           - Multiply n by 1
+           - Store result
+           - Decrement n
+           - Multiply result by n - 1
+           - Loop until n = 1
+         */
+
+        //Reverse iterate until num reaches 1
+        while (num > 1) {
+            //Multiply result by current num
+            result = num * result;
+            // Decrement num
+            num--;
+        }
+
+        /* Recursive version:
+        int getFactorial(num){
+
+            if(num==1) {
+            return num;
+            }
+            return num*getFactorial(num--);
+        }
+
+        result = getFactorial(num);
+         */
+
+        //YOUR CODE ENDS HERE
+        this.printStream.print("The Factorial is: " + result);
+
+    }
+
+    public void calculateFactorial() {
 		 /*
 		 Do not change this method.
 		 */
@@ -75,14 +117,14 @@ public class Factorial {
 	 }
 	
 
-	 public static void main(String[] args) { 
+    public static void main(String[] args) {
 		/*
 		 Do not change this method.
 		 */
-		Factorial fact = new Factorial(System.in,System.out);
-		System.out.println("Enter an integer between 1 and 10, inclusive.");
-		fact.calculateFactorial();	
-	 }
+        Factorial fact = new Factorial(System.in, System.out);
+        System.out.println("Enter an integer between 1 and 10, inclusive.");
+        fact.calculateFactorial();
+    }
 
-	
+
 }
